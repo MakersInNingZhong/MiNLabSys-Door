5fc8a153ef623338738add96366164f752eeba54
LEAFERx <jerryleafer@outlook.com>
2017-05-24 11:32:53 +0800

    Add ESP8266 Init

--- ESP8266.cpp
+++ ESP8266.cpp
@@ -7,5 +7,41 @@ SoftwareSerial esp8266(ESP8266_RX, ESP8266_TX);
 #endif
 
 void InitESP8266(){
+    #ifdef USE_SOFTWARE_SERIAL
+        Serial.println("###### Initializing ESP8266 ######");
+        Serial.println("SET BAUD:" + String(ESP8266_BAUD));
+    #endif
     ESP8266.begin(ESP8266_BAUD);
+    delay(20);
+    #ifdef USE_SOFTWARE_SERIAL
+        Serial.println("RESET ESP8266");
+    #endif
+    ESP8266.println("AT+RST");
+    String rstmsg = ReadESP8266Data().trim();
+    while(!rstmsg.endsWith("GOT IP")){
+        delay(500);
+        String rstmsg = ReadESP8266Data().trim();
+    }
+    #ifdef USE_SOFTWARE_SERIAL
+        Serial.println("ESP8266 HAS GOT IP");
+        Serial.print("IP: ");
+        ESP8266.println("AT+CIFSR");
+        String ipmsg = ReadESP8266Data().trim();
+        if(ipmsg.endsWith("OK")){
+            ipmsg = ipmsg.remove(0,8);
+            ipmsg = ipmsg.remove(ipmsg.indexOf("OK"));
+            Serial.println(ipmsg.trim());
+        }
+        Serial.println("ESP8266 Initialized!");
+    #endif
+}
+
+String ReadESP8266Data(){
+    while(!ESP8266.available());
+    String temp;
+    while(ESP82666.available()){
+        temp += (char)ESP8266.read();
+        delay(4);
+    }
+    return temp;
 }
--- ESP8266.h
+++ ESP8266.h
@@ -9,17 +9,18 @@
 #define ESP8266_BAUD 9600
 
 #ifdef USE_SOFTWARE_SERIAL
-
-#include <SoftwareSerial.h>
-extern SoftwareSerial esp8266;
-#define ESP8266 esp8266
-
+    #include <SoftwareSerial.h>
+    extern SoftwareSerial esp8266;
+    #define ESP8266 esp8266
 #else
-
-#define ESP8266 Serial
-
+    #define ESP8266 Serial
 #endif
 
+#define SERVER_IP "192.168.1.68"
+#define SERVER_PORT 3000
+
 void InitESP8266();
+
+String ReadESP8266Data();
 
 #endif
