f2b9891d9463581d8302f8dd79e7daf850139cd5
LEAFERx <jerryleafer@outlook.com>
2017-05-24 18:23:54 +0800

    Debug ESP8266_Init

--- ESP8266.cpp
+++ ESP8266.cpp
@@ -9,7 +9,7 @@ SoftwareSerial esp8266(ESP8266_RX, ESP8266_TX);
 String ESP8266_ReadData() {
     while(!ESP8266.available());
     String temp;
-    while(ESP82666.available()){
+    while(ESP8266.available()){
         temp += (char)ESP8266.read();
         delay(4);
     }
@@ -18,8 +18,9 @@ String ESP8266_ReadData() {
 
 void ESP8266_Init() {
     #ifdef USE_SOFTWARE_SERIAL
+        Serial.begin(115200);
         Serial.println("###### Initializing ESP8266 ######");
-        Serial.println("SET BAUD:" + String(ESP8266_BAUD));
+        Serial.println("SET BAUD: " + String(ESP8266_BAUD));
     #endif
     ESP8266.begin(ESP8266_BAUD);
     delay(20);
@@ -27,45 +28,54 @@ void ESP8266_Init() {
         Serial.println("RESET ESP8266");
     #endif
     ESP8266.println("AT+RST");
-    String rst_msg = ESP8266_ReadData().trim();
+    String rst_msg = ESP8266_ReadData();
+    rst_msg.trim();
     while(!rst_msg.endsWith("GOT IP")){
         delay(500);
-        String rst_msg = ESP8266_ReadData().trim();
+        rst_msg = ESP8266_ReadData();
+        rst_msg.trim();
     }
     #ifdef USE_SOFTWARE_SERIAL
         Serial.println("ESP8266 HAS GOT IP");
-        Serial.print("IP: ");
         ESP8266.println("AT+CIFSR");
-        String ip_msg = ESP8266_ReadData().trim();
-        if(i_pmsg.endsWith("OK")){
-            ip_msg = ip_msg.remove(0,8);
-            ip_msg = ip_msg.remove(ip_msg.indexOf("OK"));
-            Serial.println(ip_msg.trim());
+        String ip_msg = ESP8266_ReadData();
+        ip_msg.trim();
+        if(ip_msg.endsWith("OK")){
+            ip_msg.remove(ip_msg.indexOf("OK"));
+            ip_msg.trim();
+            Serial.print("IP: ");
+            ip_msg.remove(0, ip_msg.indexOf("\"") + 1);
+            Serial.println(ip_msg.substring(0, ip_msg.indexOf("\"")));
+            ip_msg.remove(0, ip_msg.indexOf("\"") + 1);
+            Serial.print("MAC: ");
+            ip_msg.remove(0, ip_msg.indexOf("\"") + 1);
+            Serial.println(ip_msg.substring(0, ip_msg.indexOf("\"")));
         }
         Serial.println("ESP8266 Initialized!");
     #endif
 }
 
-String CreateHTTPHeader(String method, String path, String content_type, String content_length){
+String CreateHTTPHeader(String method, String path, String content_type, int content_length){
     //String header = method + " " + path+ " HTTP/1.1\r\n" + ...
 }
 
 String CreatePOSTRequest(String path, String content_type, String body) {
-    String header = CreateHTTPHeader("POST", path, content_type);
+    String header = CreateHTTPHeader("POST", path, content_type, body.length());
 }
 
 void ESP8266_Send_Json(String json_str) {
-    String req = CreateHTTPRequest();
+    //String req = CreatePOSTRequest();
 }
 
 String CertificateUID(byte uid1, byte udi2, byte uid3, byte uid4) {
     String uid_json; //package uid into json
     ESP8266_Send_Json(uid_json);
     delay(200);
-    String cer_msg = ESP8266_ReadData().trim();
+    String cer_msg = ESP8266_ReadData();
+    cer_msg.trim();
     //unpackge cer_msg
     if(true){  //if Certificated
-        return name;
+        return String("");
     }
     else{
         return String("");
