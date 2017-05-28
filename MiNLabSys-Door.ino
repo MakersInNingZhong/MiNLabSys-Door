fccb5db5236d73934a1e98fc7040e672ef2daeee
LEAFERx <jerryleafer@outlook.com>
2017-05-28 08:09:07 +0800

    finished main file

--- MiNLabSys-Door.ino
+++ MiNLabSys-Door.ino
@@ -1,13 +1,21 @@
 #include "ESP8266.h"
+#include "RFID.h"
 
 void setup() {
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
-  ESP8266_Init();
-  CertificateUID(1,2,3,4);
+    ESP8266_Init();
+	RFID_Init();
 }
 
 void loop() {
-  
+	byte *uid = RFID_ReadUID();
+	if(uid != nullptr){
+		String user_name = CertificateUID(uid[0], uid[1], uid[2], uid[3]);
+		if(user_name != String("")){
+			#ifdef USE_DEBUG_MODE
+				DEBUG.println(user_name + " has opened the door!");
+			#endif
+			// Open the door & display name
+		}
+	}
 }
 
