71b4a4a874331e1889125d63d1edfdfa631f458b
LEAFERx <jerryleafer@outlook.com>
2017-05-28 08:08:27 +0800

    Finished RFID part

--- RFID.cpp
+++ RFID.cpp
@@ -1,3 +1,52 @@
 #include <Arduino.h>
 
 #include "RFID.h"
+
+MFRC522 RFID(MFRC522_SDA_PIN, MFRC522_RST_PIN);
+
+void RFID_Init() {
+    SPI.begin();
+    RFID.PCD_Init();
+    #ifdef USE_DEBUG_MODE
+        DEBUG.println("###### Initializing MFRC522 ######");
+        DEBUG.println("SDA is attached to: " + String(MFRC522_SDA_PIN));
+        DEBUG.println("RST is attached to: " + String(MFRC522_RST_PIN));
+        RFID.PCD_DumpVersionToSerial();
+        DEBUG.println("MFRC522 Initialized!");
+    #endif
+}
+
+#ifdef USE_DEBUG_MODE
+void dump_byte_array(byte *buffer, byte bufferSize) {
+    for (byte i = 0; i < bufferSize; i++) {
+        DEBUG.print(buffer[i] < 0x10 ? " 0" : " ");
+        DEBUG.print(buffer[i], HEX);
+    }
+}
+#endif
+
+byte * RFID_ReadUID() {
+    if (RFID.PICC_IsNewCardPresent() && RFID.PICC_ReadCardSerial()){
+        byte *uids = RFID.uid.uidByte;
+        MFRC522::PICC_Type piccType = RFID.PICC_GetType(RFID.uid.sak);
+        MFRC522::PICC_Type expectedType = RFID.PICC_GetType(0x08); //PICC_TYPE_MIFARE_1K in MFRC522.cpp line 1276
+        if (piccType != expectedType){
+            return nullptr;
+        }
+        #ifdef USE_DEBUG_MODE
+            DEBUG.print("Card UID:");
+            dump_byte_array(RFID.uid.uidByte, RFID.uid.size);
+            DEBUG.println();
+            DEBUG.print("PICC type: ");
+            DEBUG.println(RFID.PICC_GetTypeName(piccType));
+        #endif
+
+        RFID.PICC_HaltA();
+        RFID.PCD_StopCrypto1();
+
+        return uids;
+    }
+    else{
+        return nullptr;
+    }
+}
--- RFID.h
+++ RFID.h
@@ -4,4 +4,12 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
+#define MFRC522_RST_PIN 9
+#define MFRC522_SDA_PIN 10
+
+extern MFRC522 RFID;
+
+void RFID_Init();
+byte * RFID_ReadUID();
+
 #endif
